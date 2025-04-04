ec30322a7fef14b0a69298b30a603b964c8a47c9
Nick <nodhiambo01@gmail.com>
2025-04-04 13:38:35 +0300

    updates

--- truckerapp/settings.py
+++ truckerapp/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = "truckerapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
